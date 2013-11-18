73fb8027576df3bf16dd1e92a7df6a2473c406d3
mulleady1 <kyle.mulleady@gmail.com>
2013-11-17 21:05:38 -1000

    Adds Constants.java

---
+++ src/ics311km2/Constants.java
@@ -0,0 +1,14 @@
+package ics311km2;
+
+public interface Constants {
+	static final String IN_DEGREE_MIN  = "IN_DEGREE_MIN";
+	static final String IN_DEGREE_MAX  = "IN_DEGREE_MAX";
+	static final String IN_DEGREE_AVE  = "IN_DEGREE_AVE";
+	static final String OUT_DEGREE_MIN = "OUT_DEGREE_MIN";
+	static final String OUT_DEGREE_MAX = "OUT_DEGREE_MAX";
+	static final String OUT_DEGREE_AVE = "OUT_DEGREE_AVE";
+	static final String DENSITY  = "DENSITY";
+	static final String FILENAME = "FILENAME";
+	static final String NUM_SCC  = "NUM_SCC";
+	static final String PERCENT  = "PERCENT";
+}
--- src/ics311km2/Driver.java
+++ src/ics311km2/Driver.java
@@ -10,19 +10,9 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 
-public class Driver {
+public class Driver implements Constants {
 
 	private static Map<String, Object> data = new HashMap<String, Object>();
-	static final String IN_DEGREE_MIN  = "IN_DEGREE_MIN";
-	static final String IN_DEGREE_MAX  = "IN_DEGREE_MAX";
-	static final String IN_DEGREE_AVE  = "IN_DEGREE_AVE";
-	static final String OUT_DEGREE_MIN = "OUT_DEGREE_MIN";
-	static final String OUT_DEGREE_MAX = "OUT_DEGREE_MAX";
-	static final String OUT_DEGREE_AVE = "OUT_DEGREE_AVE";
-	static final String DENSITY  = "DENSITY";
-	static final String FILENAME = "FILENAME";
-	static final String NUM_SCC  = "NUM_SCC";
-	static final String PERCENT  = "PERCENT";
 	
 	public static void main(String[] args) throws IOException, FileNotFoundException {
 		String filename = args[0];
